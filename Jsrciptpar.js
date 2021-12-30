69910aa81c263793428de2a74165d972943b9c37
Trevino20 <96582345+Trevino20@users.noreply.github.com>
2021-12-30 14:14:06 +0530

    Add files via upload

---
+++ Jsrciptpar.js
@@ -0,0 +1,17 @@
+"use strict";
+// in line number 16 17 the code is error becouse the variable data is not used and 
+// by used of the use strict its complasarie to use the data type otherwise it thought error
+console.log("hello world"); 
+// console open in brosser shortcut key is clrl+shift+J
+// console.log are used to print the program in the console brosser 
+// shortcut key to comment the stetment is ctrl+/ to uncomment is same 
+// variable can store some information 
+// we can use that information later
+// we can change that information later 
+// declare a variable   
+console.log(name);
+var nameofthedeparment = "Avadhut varvatkar is a good boy";
+console.log(nameofthedeparment);
+// avadhut = "avadhut";
+// console.log(avadhut);
+      
